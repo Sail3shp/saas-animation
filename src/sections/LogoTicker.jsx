ea7a62a01ba4054e52f99c58cf71f28cfb7d020f
Sail3shp <saileshpokhrel557@gmail.com>
2026-04-26 21:25:39 +0545

    logo ticker animation

--- src/sections/LogoTicker.jsx
+++ src/sections/LogoTicker.jsx
@@ -4,25 +4,36 @@ import celestial from '../assets/logo-celestial.png'
 import echoLogo from '../assets/logo-echo.png'
 import quantum from '../assets/logo-quantum.png'
 import pulseLogo from '../assets/logo-pulse.png'
+import { motion } from 'motion/react'
 
 const LogoTicker = () => {
-    const logos = [acemLogo, apexLogo, celestial, echoLogo, quantum, pulseLogo]
+    const logos = [acemLogo, apexLogo, celestial, echoLogo, quantum, pulseLogo,acemLogo, apexLogo, celestial, echoLogo, quantum, pulseLogo]
     logos.map((logo) => console.log(logo))
     return (
         <section className='py-20 px-4 md:py-24'>
             <div className="flex  items-center gap-5 ">
                 <div className='flex-1 md:flex-none'>
-                    <h2>
+                    <h2 className='text-white/70'>
                         Trusted by top innovative teams
                     </h2>
                 </div>
                 <div className='flex-1 overflow-hidden mask-[linear-gradient(to_right,transparent,black_20%,transparent)]'>
-                    <div className='flex flex-none gap-14 '>
+                    <motion.div 
+                    initial={{
+                        translateX: '-50%'
+                    }}
+                    animate={{ translateX: '0'}}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 60,
+                        ease: 'linear'
+                    }}
+                    className='flex flex-none gap-14 '>
                         {logos.map((logo) => (
                             <img src={logo} key={logo} className='h-6 w-atuo' />
                         ))}
 
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
