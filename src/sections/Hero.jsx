5ea37c15a09d7363dd81adc581a5933e9a5d222a
Sail3shp <saileshpokhrel557@gmail.com>
2026-04-26 20:26:48 +0545

    hero animated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,33 +1,87 @@
 import Button from "../components/Button"
 import starsbg from '../assets/stars.png'
+import { motion,useMotionValueEvent,useScroll, useTransform } from "motion/react"
+import { useRef } from "react"
 const Hero = () => {
+    const sectionRef = useRef(null)
+    const {scrollYProgress }= useScroll({
+        target: sectionRef,
+        offset: ["start end","end start"]
+    })
+    const backgroundPositionY = useTransform(scrollYProgress,[0,1],[-300,300])
+
+
     return (
-        <section
+        <motion.section
+        ref={sectionRef}
             className="h-150 md:h-200  flex items-center overflow-hidden relative mask-[linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
             style={{
-                backgroundImage: `url(${starsbg})`
+                backgroundImage: `url(${starsbg})`,
+                backgroundPositionY,
+            }}
+            animate={{
+                backgroundPositionX: ["0px" , "1000px"],
+            }}
+            transition={{
+                repeat: Infinity,
+                duration: 120,
+                ease:"linear"
             }}
         >
             <div className="absolute inset-0 bg-[radial-gradient(75%_75%__at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
 
             <div className="absolute size-64 md:size-96 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)]"></div>
 
-            <div className="absolute size-86 md:size-145 rounded-full border opacity-20 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+            <motion.div 
+                animate={{
+                rotate: '1turn',
+            }} 
+            transition={{
+                duration: 30,
+                repeat: Infinity,
+                ease: "linear"
+            }}
+            className="absolute size-86 md:size-145 rounded-full border opacity-20 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
 
                 <div className="absolute size-2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
                 <div className="absolute size-2 top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
                 <div className="absolute size-5 top-1/2 border border-white left-full   -translate-x-1/2 -translate-y-1/2 rounded-full flex items-center justify-center">
                     <div className="size-2 bg-white rounded-full "></div>
                 </div>
-            </div>
-            <div className="absolute size-111 md:size-195 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-            <div className="absolute size-136 md:size-245 border border-white rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+            </motion.div>
+            <motion.div 
+            animate={{
+                rotate: '-1turn',
+            }}
+            transition={{
+                duration: 60,
+                repeat: Infinity,
+                ease:"linear"
+
+            }}
+            className="absolute size-111 md:size-195 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"></motion.div>
+
+            <motion.div 
+            animate={{
+                rotate: '1turn',
+            }}
+            transition={{
+                duration: 90,
+                repeat: Infinity,
+                ease:"linear"
+
+            }}
+            className="absolute size-136 md:size-245 border border-white/40 rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="absolute size-2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
+                <div className="absolute size-2 top-1/2 left-full -translate-x-1/2 -translate-y-1/2 rounded-full bg-white"></div>
+            </motion.div>
+
             <div className=" relative mt-16 mx-auto">
                 <h1 className="text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] bg-clip-text text-transparent text-center">AI SEO</h1>
                 <p className="text-lg md:text-xl max-w-xl mx-auto text-white/70 mt-5 text-center">Elevate your site's visibility effortlessly with AI, where smart technology meets user-friendly SEO tools</p>
                 <div className="flex justify-center mt-5"> <Button>Join Waitlist</Button></div>
             </div>
-        </section>
+        </motion.section>
     )
 }
 
