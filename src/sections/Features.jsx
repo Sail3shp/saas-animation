1753e9e94ba911dbc0d1bb266a715d4b7d027b9f
Sail3shp <saileshpokhrel557@gmail.com>
2026-04-27 12:52:37 +0545

    animated tabs

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -4,65 +4,119 @@ import click from '../assets/lottie/click.lottie'
 import stars from '../assets/lottie/stars.lottie'
 import productImage from '../assets/product-image.png'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { useEffect, useRef } from "react";
+import {motion, useMotionTemplate, useMotionValue } from "motion/react";
+import { animate } from "motion";
 //could have used public folder for lottie files might have been easier
 
 const tabs = [
     {
         icon: vroom,
-        title:'User-friendly dashboard',
+        title: 'User-friendly dashboard',
         isNew: false,
         backgroundPositionX: 0,
         backgroundPositionY: 0,
         backgroundSizeX: 150,
     },
     {
-       icon:click,
-        title:'One-click optimization',
+        icon: click,
+        title: 'One-click optimization',
         isNew: false,
         backgroundPositionX: 98,
         backgroundPositionY: 100,
-        backgroundSizeX: 135, 
+        backgroundSizeX: 135,
     },
     {
-        icon:stars,
-        title:'Smart keyword generator',
+        icon: stars,
+        title: 'Smart keyword generator',
         isNew: true,
         backgroundPositionX: 100,
         backgroundPositionY: 27,
         backgroundSizeX: 177,
     }
 ]
+
+const FeatureTab = (tab) => {
+    const tabRef = useRef(null)
+    const dotLottieRef = useRef(null)
+
+    const xPercentage = useMotionValue(100)
+    const yPercentage = useMotionValue(50)
+
+    const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPercentage}% ${yPercentage}% ,black,transparent)`
+
+    useEffect(() => {
+        if(!tabRef.current) return
+        const {height ,width} = tabRef.current?.getBoundingClientRect()
+        const cirumference = height * 2 + width * 2
+
+        const times = [0,width / cirumference, (width + height) / cirumference,(width * 2 + height)/cirumference,1]
+
+        const options = {
+            times,
+            duration: 4,
+            repeat: Infinity,
+            ease:"linear",
+            repeatType: "loop",
+        }
+        animate(xPercentage,[0,100,100,0,0],options)
+        animate(yPercentage,[0,0,100,100,0],options)
+    },[])
+
+    const handleTabHover = () => {
+        const player = dotLottieRef.current;
+        if (!player) return;
+
+        player.stop();   // more reliable reset
+        player.play();
+    }
+    return (
+        <div 
+        ref={tabRef}
+        onMouseEnter={handleTabHover} className="relative border lg:flex-1 border-white/15 flex rounded-xl p-2.5 gap-2.5 items-center">
+            <motion.div
+            style={{
+                maskImage
+            }} className="absolute -m-px rounded-lg inset-0 border border-[#A369FF] "></motion.div>
+            <div
+                className="size-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+                <DotLottieReact
+                    dotLottieRefCallback={(instance) => {
+                        dotLottieRef.current = instance;
+                    }}
+                    src={tab.icon}
+                    className="size-5" />
+            </div>
+            <div className="font-medium">{tab.title}</div>
+            {tab.isNew && (
+                <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">new</div>
+            )
+            }
+
+        </div>
+    )
+}
 const Features = () => {
     tabs.map((tab) => console.log(tab.icon))
-  return (
-    <section className="px-4 py-20 md:py-24">
-        <h1 className="text-5xl md:text-6xl font-medium tracking-tight text-center">
-            Elevate your SEO efforts
-        </h1>
-        <p className="text-white/70 md:text-xl max-w-2xl mx-auto text-center text-lg tracking-tight mt-5">
-            From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.
-        </p>
-        <div className="mt-10 flex flex-col lg:flex-row  gap-3 ">
-        {tabs.map((tab) => (
-            <div key={tab.title} className="border lg:flex-1 border-white/15 flex rounded-xl p-2.5 gap-2.5 items-center">
-                <div className="size-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
-                <DotLottieReact src={tab.icon}  className="size-5" autoplay />
-                </div>
-                <div className="font-medium">{tab.title}</div>
-                {tab.isNew &&( 
-                    <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">new</div>
-                )
-                }
-
+    return (
+        <section className="px-4 py-20 md:py-24">
+            <h1 className="text-5xl md:text-6xl font-medium tracking-tight text-center">
+                Elevate your SEO efforts
+            </h1>
+            <p className="text-white/70 md:text-xl max-w-2xl mx-auto text-center text-lg tracking-tight mt-5">
+                From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.
+            </p>
+            <div className="mt-10 flex flex-col lg:flex-row  gap-3 ">
+                {tabs.map((tab) => (
+                    <FeatureTab {...tab} key={tab.title} />
+                ))}
             </div>
-        ))}
-        </div>
-        <div className="border border-white/20 p-2.5 rounded-xl mt-3">
-        <img src={productImage} alt="productImg" />
-        </div>
+            <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+                <img src={productImage} alt="productImg" />
+            </div>
 
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Features
