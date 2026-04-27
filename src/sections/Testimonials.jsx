487a9bbda093103ec361d737f765c1972db4e201
Sail3shp <saileshpokhrel557@gmail.com>
2026-04-27 13:17:47 +0545

    testimonial motion

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -2,6 +2,7 @@ import avatar1 from '../assets/avatar-1.png'
 import avatar2 from '../assets/avatar-2.png'
 import avatar3 from '../assets/avatar-3.png'
 import avatar4 from '../assets/avatar-4.png'
+import { motion } from 'motion/react'
 const testimonials = [
     {
         text: '"This product has completely transformed how I manage my project and deadlines"',
@@ -34,9 +35,21 @@ const Testimonials = () => {
             <h1 className="text-5xl md:text-6xl font-medium tracking-tighter text-center">Beyond Expectations.</h1>
             <p 
             className="text-center text-lg md:text-xl text-white/70  mt-5 max-w-sm mx-auto ">Our revolutionary AI SEO tools have transformed our client's strategies.</p>
-            <div className='overflow-hidden mt-10 mask-[linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
-                <div className='flex gap-5'>
-                    {testimonials.map(testimonial => (
+            <div className='flex overflow-hidden mt-10 mask-[linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
+                <motion.div
+                initial={{
+                    translateX: '-50%',
+                }}
+                animate={{
+                    translateX: '0',
+                }}
+                transition={{
+                    repeat:Infinity,
+                    ease:'linear',
+                    duration: 30,
+                }}
+                className='flex gap-5 flex-none pr-5'>
+                    {[...testimonials,...testimonials].map(testimonial => (
                         <div key={testimonial.name} className='border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md  flex-none'>
                             <div className='text-lg md:text-2xl tracking-tight'>
                                 {testimonial.text}
@@ -52,7 +65,7 @@ const Testimonials = () => {
                             </div>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     )
